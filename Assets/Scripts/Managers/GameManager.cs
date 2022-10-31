9981aed9aa48cc8bec5d4d073b0cd5a2faa1bdec
Desolate <desolate.edge@gmail.com>
2022-10-31 08:55:10 +0300

    game manager fix

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,28 +1,30 @@
-using Scripts.Managers;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+namespace Scripts.Managers
 {
-    [Header("Components")]
-    [Space(10)]
-    [SerializeField] private UIManager _uiManager;
-    [SerializeField] private ActorsManager _actorsManager;
-    [SerializeField] private WorldManager _worldManager;
-    [SerializeField] private InputManager _inputManager;
-
-    private void Awake()
+    public class GameManager : MonoBehaviour
     {
+        [Header("Components")] [Space(10)] [SerializeField]
+        private UIManager _uiManager;
+
+        [SerializeField] private ActorsManager _actorsManager;
+        [SerializeField] private WorldManager _worldManager;
+        [SerializeField] private InputManager _inputManager;
+
+        private void Awake()
+        {
 #if !UNITY_EDITOR
             Debug.unityLogger.logEnabled = false;
             Application.targetFrameRate = 60;
             QualitySettings.vSyncCount = 0;
 #endif
-    }
+        }
 
-    private void Start()
-    {
-        _uiManager.HUDController.SetClearCallback(_worldManager.ClearSurfaces);
-        _uiManager.HUDController.SetUnitAddCallback(_actorsManager.Add);
-        _uiManager.HUDController.SetUnitRemoveCallback(_actorsManager.Remove);
+        private void Start()
+        {
+            _uiManager.HUDController.SetClearCallback(_worldManager.ClearSurfaces);
+            _uiManager.HUDController.SetUnitAddCallback(_actorsManager.Add);
+            _uiManager.HUDController.SetUnitRemoveCallback(_actorsManager.Remove);
+        }
     }
 }
